5dabacaa519ba68aa4b7b8ce50d43cad24152100
Orlando <orlando.morales@capco.com>
2022-11-08 09:46:37 -0500

    first commit

---
+++ app/components/Popular.jsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { fetchPopularRepos } from '../utils/api';
+import Table from './Table';
+
+function LanguagesNav({selected, onUpdateLanguage}) {
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+        <select
+            onChange={(e) => onUpdateLanguage(e.target.value)}
+            selected={selected}
+            >
+            {languages.map((language) => (
+                <option key={language} value={language}>
+                    {language}
+                </option>
+            ))}
+        </select>
+    )
+}
+
+LanguagesNav.proptype = {
+    selected: PropTypes.string.isRequired,
+    onUpdateLanguage: PropTypes.func.isRequired
+}
+
+export default class Popular extends React.Component {
+    state = {
+        selectedLanguage: 'All',
+        repos: null,
+        errors: null
+    }
+
+    componentDidMount() {
+        this.updateLanguage(this.state.selectedLanguage)
+    }
+
+    updateLanguage = (selectedLanguage) => {
+        this.setState({
+            selectedLanguage,
+            error: null
+        });
+
+        fetchPopularRepos(selectedLanguage).then(
+            (repos) => this.setState({
+                repos, 
+                error: null
+            })
+        ).catch((error) => {
+            console.warn("Error fetching repos: ", error)
+            this.setState({
+                error: `There was an error fetching the repositories`,
+            })
+        })
+    }
+
+    render() {
+        const { selectedLanguage, repos, error } = this.state;
+
+        return (
+            <main className='stack main-stack animate-in'>
+                <div className='split'>
+                    <h1>Popular</h1>
+                    <LanguagesNav
+                    selected={selectedLanguage}
+                    onUpdateLanguage={this.updateLanguage}/>
+                    {/* { JSON.stringify(this.state, null, 2) } */}
+               </div>
+               {error && <p className='text-center error'>{error}</p>}
+               {/* {repos && <pre>{JSON.stringify(this.state, null, 2)}</pre>} */}
+               {repos && <Table repos={repos}/>}
+            </main>
+        )
+
+    }
+}
+
